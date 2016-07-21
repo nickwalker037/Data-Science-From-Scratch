5f429211c9f27735f8e3b144e7dfc86c128aae04
nickwalker037 <nickwalker037@gmail.com>
2016-07-21 19:38:42 -0400

    Create Dispersion.py

---
+++ Chapter-5---Statistics/Dispersion.py
@@ -0,0 +1,42 @@
+# ----- DISPERSION ----- #
+
+# Range
+    # Note: "range" already means something else in Python, so we'll use a different name
+    
+def data_range(x):
+    return max(x) - min(x)
+data_range(num_friends)                                     # 99
+
+# Variance
+def de_mean(x):
+    """translate x by subtracting its mean (so the result has mean 0)"""
+    x_bar = mean(x)
+    return [x_i - x_bar for x_i in x]
+def variance(x):
+    """ assumes x has at least two elements"""
+    n = len(x)
+    deviations = de_mean(x)
+    return sum_of_squares(deviations) / (n - 1)
+variance(num_friends)                                       # 81.54
+
+# Standard Deviation
+
+def standard_deviation(x):
+    return math.sqrt(variance(x))
+standard_deviation(num_friends)                             # 9.03
+
+# Outlier problem: if our highest value was 200, our st. dev = 14.89
+# to solve the outlier problem with st. dev, we could calculate it using the 25th and 75th percentile values
+def interquartile_range(x):
+    return quantile(x, 0.75) - quantile(x, 0.25)
+interquartile_range(num_friends)                            # 6
+
+
+# ----- CORRELATION ----- #
+
+# Covariance
+# whereas variance measures how a single variable deviates from its mean, covariance measures how two variables vary in tandem from their means
+def covariance(x, y):
+    n = len(x)
+    return dot(de_mean(x), de_mean(y)) / (n - 1)
+covariance(num_friends, daily_minutes)                      # 22.43
