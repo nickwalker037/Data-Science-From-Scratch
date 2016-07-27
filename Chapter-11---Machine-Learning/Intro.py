7e92078f43fa50de816039e9accdf718998f80bf
nickwalker037 <nickwalker037@gmail.com>
2016-07-27 18:24:23 -0400

    Create Intro.py

---
+++ Chapter-11---Machine-Learning/Intro.py
@@ -0,0 +1,17 @@
+# Model - a specification of a mathematical (or probabilistic) relationship that exists between different variables
+
+# Machine Learning - refers to creating and using models that are learned from data
+    # - can also be called predictive modeling or data mining
+# Ex.
+    # - Predicting if an email is considered spam or not
+    # - Predicting whether a credit card transaction is fraudulent
+    # - Predicting which advertisement a shopper is most likely to click on
+    # - Predicting which football team will win the super bowl
+
+# 4 Model Types:
+    # Supervised Model - There is a set of data labeled with correct answers to choose from
+    # Unsupervised Model - There are no such labels
+    # Semisupervised Models - Only some of the data are labeled
+    # Online Models - The model needs to continuously adjust to newly arriving data
+
+# in most cases, we will choose a parameterized family of models and then use data to learn parameters that are in some way optimal
