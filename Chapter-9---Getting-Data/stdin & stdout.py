288c09c5094dc82a2dafc97f57a16bafad4e74c0
nickwalker037 <nickwalker037@gmail.com>
2016-07-26 23:46:57 -0400

    Update stdin & stdout.py

--- Chapter-9---Getting-Data/stdin & stdout.py
+++ Chapter-9---Getting-Data/stdin & stdout.py
@@ -14,3 +14,46 @@ for line in sys.stdin:
     # if it matches the regex, write it to stdout
     if re.search(regex, line):;
     sys.stdout.write(line)
+
+# line_count.py
+import sys
+
+count = 0
+for line in sys.stdin:
+    count += 1
+
+# print goes to sys.stdout
+print count
+
+# THEN... you could use these to count how many lines of a file contains numbers
+# in windows:
+type SomeFile.txt | python egrep.py "[0-9]" | python line_count.py
+# in Unix:
+cat SomeFile.txt | python egrep.py "[0-9]" | python line_count.py
+
+# Note: the | is the pip character, which means "use the output of the left command as the input of the right command"
+#       - you could build pretty elaborate data-processing pipelines this way
+
+
+# Here's a script that counts the words in its input and writes out the most common ones:
+# most_common_words.py
+import sys
+from collections import Counter
+
+# pass in number of words as first argument
+try:
+    num_words = int(sys.argv[1])
+except:
+    print "usage: most_common_words.py num_words"
+    sys.exit(1)                                             # non-zero exit code indicates error
+
+counter = Counter(word.lower()                              # lowercase words
+                  for line in sys.stdin
+                  for word in line.strip().split()          # split on space
+                  if word)                                  # skip empty 'words'
+
+for word, count in counter.most_common(num_words):
+    sys.stdout.write(str(count))
+    sys.stdout.write("\t")
+    sys.stdout.write(word)
+    sys.stdout.write("\n")
