75f2e459fb5ebfd963f21f5b2e969ef322dbb9f3
nickwalker037 <nickwalker037@gmail.com>
2016-07-27 23:44:46 -0400

    Create Feature Extraction and Selection.py

---
+++ Chapter-11---Machine-Learning/Feature Extraction and Selection.py
@@ -0,0 +1,7 @@
+# ----- FEATURE EXTRACTION AND SELECTION ----- # 
+
+# As discussed before:
+# when your data doesn't have enough features, your model is likely to underfit
+# when your data has too many features, it's easy to overfit
+# But what are features and where do they come from?
+# FEATURES: are whatever inputs we provide to our model
