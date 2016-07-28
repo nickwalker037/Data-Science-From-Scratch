416c3af8547f283569ecce51c1c45c97a9756186
nickwalker037 <nickwalker037@gmail.com>
2016-07-27 23:43:21 -0400

    Update The Rest.py

--- Chapter-11---Machine-Learning/The Rest.py
+++ Chapter-11---Machine-Learning/The Rest.py
@@ -52,20 +52,6 @@ return 2 * p * r  (p + r)
 # you could think of this as a trade-off between false positives and false negatives
 
 
-# ----- THE BIAS VARIANCE TRADEOFF ----- # 
-
-# is just another way of thinking about the overfitting problem 
-
-# if your model has high bias (which means it performs poorly even on your training data) then one thing to try is adding more features
-# if your model has high variance, then you can similarly remove features. But another solution is to obtain more data (if you can)
-
-
-# holding model complexity constant, the more data you have, the harder it is to overfit
-# on the other hand, more data won't help with bias
-# if your model doesn't use enough features to capture regularities in the data, throwing more data at it won't help
-
-
-
 # ----- FEATURE EXTRACTION AND SELECTION ----- # 
 
 # As discussed before:
